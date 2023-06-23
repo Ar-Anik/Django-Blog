8ef4cfec10e8bcb6fe89a8f531093ddb22f16599
Aubdur Rob Anik <aubdurrob1331@gmail.com>
2023-06-23 15:08:18 +0600

    ADD <Custom User> Custom User Model Create

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'user_profile',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user_profile.User'
+
---
+++ django_blog/user_profile/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .manager import CustomUserManager
+
+# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=150, unique=True,
+                              error_messages={
+                                  "unique": "Email Must Be Unique"
+                              })
+    profile_image = models.ImageField(upload_to="profile_images", null=True, blank=True)
+
+    REQUIRED_FIELDS = ["email"]
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
