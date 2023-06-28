c1a243eed7330d7fd0219a655938a96c0eacafb6
Aubdur Rob Anik <aubdurrob1331@gmail.com>
2023-06-28 11:29:21 +0600

    Modify <All Blog> Modify All Blog Page

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
 from .models import Blog, Tag
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 # Create your views here.
 
 def home(request):
-    blog = Blog.objects.order_by('-created_date')
+    blogs = Blog.objects.order_by('-created_date')
     tags = Tag.objects.all()
     context = {
-        "blogs": blog,
+        "blogs": blogs,
         "tags": tags
     }
     return render(request, 'home.html', context)
 
 def blogs(request):
-    return render(request, 'blogs.html')
+    data = Blog.objects.order_by('-created_date')
+    tags = Tag.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(data, 4)
+
+    try:
+        blogs = paginator.page(page)
+    except EmptyPage:
+        blogs = paginator.page(1)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+
+    context = {
+        "blogs": blogs,
+        "blog_recent_posts": data,
+        "tags": tags,
+    }
+
+    return render(request, 'blogs.html', context)
+
